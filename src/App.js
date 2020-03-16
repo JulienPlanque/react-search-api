b818e5b40ef3738495a4415623a823ce2e926c36
Julien <julien.planque-ext@galec.fr>
2020-03-16 15:45:08 +0100

    feat: add search api with useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  // similaire à "compentDisMount" et "componentDidUpdate"
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://jsonplaceholder.typicode.com/posts",
+      );
+      setData(result.data);
+      console.log("updated");
+    };
+    fetchData();
+    console.log("mounted");
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <p>{item.title}</p>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  )
+
 }
 
 export default App;
